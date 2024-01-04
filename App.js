9e081ed4dc6106a5f9cb06824dee333bd21ba08c
Raama Batista <batistaraama914@gmail.com>
2024-01-04 00:02:04 -0300

    Your commit message

---
+++ App.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.set('views','./views')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+app.listen(port,()=>{
+    console.log(` Server listening on ${port}`)
+})
