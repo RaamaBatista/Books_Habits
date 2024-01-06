18b0e9112656fdb8a59590277c4987d2069c5128
Raama Batista <batistaraama914@gmail.com>
2024-01-05 22:35:28 -0300

    projeto 3.0

--- App.js
+++ App.js
@@ -1,38 +1,42 @@
-
 const express = require('express')
 const bodyparser = require('body-parser')
 
 const app = express()
 const port = 3000
 
-app.set('views','./views')
+var table = []
+
+app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) =>{
-    res.render('form')
+app.get('/', (req, res) => {
+    res.render('cadastro')
+    // res.render('tabela', { table });
     // res.render('index')
 })
 
-app.post('/cadastro',(req,res)=>{
+
+app.post('/cadastro', (req, res) => {
     let nome = req.body.nome
     let nomeL = req.body.nomeL
     let DnomeL = req.body.DnomeL
     let dateObject = new Date(DnomeL);
     let date =
-        `${dateObject.getDate()}/${dateObject.getMonth() + 1}/${dateObject.getFullYear()} ` +
-        `${dateObject.getHours()}:${dateObject.getMinutes()}`;
-
-    let optradio = req.body.optradio
-    res.send(`O nome do cliente é: ${nome}, o livro escolhido foi: ${nomeL}, A data do aluguel foi: ${date},A quantidade de livros alugada foi: ${optradio}`)
-   
+    `${dateObject.getDate()}/${dateObject.getMonth() + 1}/${dateObject.getFullYear()} ` +
+    `${dateObject.getHours()}:${dateObject.getMinutes()}`;
     
+    let optradio = req.body.optradio
+    let item={'nome': nome, 'LivroNome': nomeL, 'DataAluguel': date, 'QtdDia': optradio}
+
+    table.push(item)
+    res.render('tabela', {data:table});
+   
+    // res.send(`O nome do cliente é: ${nome}, o livro escolhido foi: ${nomeL}, A data do aluguel foi: ${date},A quantidade de livros alugada foi: ${optradio}`)
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(` Server listening on ${port}`)
 })
-
-
