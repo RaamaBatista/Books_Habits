87573600c7623648a090e20f5fa93e0a0406a0c6
Raama Batista <batistaraama914@gmail.com>
2024-01-11 17:45:13 -0300

    atualizações 5.0

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 
-
 const express = require('express')
 const bodyparser = require('body-parser')
 
@@ -9,6 +8,7 @@ const port = 3000
 var table = []
 var banco = []
 
+
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -16,12 +16,22 @@ app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.render('cadastro')
-    // res.render('tabela', { table });
-    // res.render('index')
-    // res.render('login')
+    res.render('login');  
 })
+app.get('/index', (req, res) => {
+    res.render('index');  
+})
+app.get('/tabela', (req, res) => {
+    res.render('tabela', { data: table });  
+});
 
+app.get('/user', (req, res) => {
+    res.render('user', { banco: banco });  
+});
+
+app.get('/cadastro', (req, res) => {
+    res.render('cadastro');  
+});
 app.post('/cadastro', (req, res) => {
     let nome = req.body.nome
     let nomeL = req.body.nomeL
@@ -32,7 +42,7 @@ app.post('/cadastro', (req, res) => {
         `${dateObject.getHours()}:${dateObject.getMinutes()}`;
 
     let optradio = req.body.optradio
-    let item = { 'nome': nome, 'LivroNome': nomeL, 'DataAluguel': date, 'QtdDia': optradio }
+    let item = { 'nome': nome, 'LivroNome': nomeL, 'DataAluguel': date, 'QtdDia': optradio, 'dataa': DnomeL }
 
     table.push(item)
     res.render('tabela', { data: table });
@@ -44,7 +54,9 @@ app.post('/cadastro', (req, res) => {
 app.post('/cada', (req, res) => {
     let email = req.body.email
     let senha = req.body.pswd
-    let item = { 'email': email, 'senha': senha }
+    let id = banco.length
+
+    let item = { 'id': id, 'email': email, 'senha': senha }
     banco.push(item)
     res.render('login')
 
@@ -57,7 +69,7 @@ app.post('/login', (req, res) => {
         for (let j = 0; j < banco.length; j++) {
             if (banco[j].email == email) {
                 if (banco[j].senha == senha) {
-                    res.render('cadastro')
+                    res.render('index')
                 } else {
                     res.send('Senha incorreta');
                 }
@@ -71,14 +83,86 @@ app.post('/login', (req, res) => {
     }
 
 })
-app.post('/editar', (req, res) => {
-  res.send('helolo')
+app.get('/user', (req, res) => {
+    res.render('user', { banco: banco });
+});
+app.post('/editaruser', (req, res) => {
+    let email = req.body.emailuser
+    console.log(email)
+    let senha = req.body.senhauser
+    console.log(senha)
+    let id =parseInt( req.body.iduser)
+    console.log(banco)
+
+    banco.forEach((item, index) => {
+        if (item.id === id) {
+          banco[index] = { ...item, email: email, senha: senha};
+          console.log('Email modificado com sucesso:', banco[index]);
+        }
+      });
+      
+      console.log('Array Atualizado:', banco);
+
+
+    res.render('user', { banco: banco });
+
 })
 
+app.post('/deleteuser', (req, res) => {
+    let id = req.body.id;
+    console.log(banco);
 
-  
-  app.listen(port, () => {
+    if (id >= 0 && id < banco.length) {
+        banco.splice(id, 1);
+        console.log('Usuário removido com sucesso. Novo array:', banco);
+        res.redirect('cadastro', { banco: banco });  //Não sei se está apagando ou nao
+
+    }
+    
+})
+app.post('/editar', (req, res) => {
+    let id = req.body.id
+    let name = req.body.recepNome
+    let livro = req.body.recepLivro
+    let data = req.body.recepDateL
+    let dias = req.body.recepDate
+
+    console.log("ID:", id);
+    console.log("Nome:", name);
+    console.log("Livro:", livro);
+    console.log("Data:", data);
+    console.log("Dias:", dias);
+    let dateObject = new Date(data);
+    let date =
+        `${dateObject.getDate()}/${dateObject.getMonth() + 1}/${dateObject.getFullYear()} ` +
+        `${dateObject.getHours()}:${dateObject.getMinutes()}`;
+
+
+    table[id].nome = name
+    table[id].LivroNome = livro
+    table[id].DataAluguel = date
+    table[id].QtdDia = dias
+
+    res.render('tabela', { data: table });
+})
+
+app.post('/deletar', (req, res) => {
+    let id = req.body.idd;
+    console.log(table);
+
+    if (id >= 0 && id < table.length) {
+        table.splice(id, 1);
+        res.render('tabela', { data: table });
+
+    }
+});
+
+
+
+app.listen(port, () => {
     console.log(` Server listening on ${port}`)
 })
+
+
 
 
